be29fc23a371a6ffd88b107d7c1d7ca85fe2dbf7
ataozkn <ataozkan_97@hotmail.com>
2022-08-17 12:59:07 +0300

    axios ve style components eklendi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Navbar() {
   return (
-    <div className='nav'>
+    <container className='nav'>
   
     <a className="site-title" href="/">Site Name</a>
     <ul>
@@ -17,8 +18,36 @@ function Navbar() {
 
     
   
-  </div>
+  </container>
   )
 }
+
+const container = styled.div`
+position: sticky;
+top:0;
+background-color: black;
+color:  white;
+display: flex;
+justify-content: space-between;
+align-items: stretch;
+gap: 2rem;
+
+padding:10px 5px 10px 5px;
+a {
+  color: inherit;
+  text-decoration: none;
+}
+ul {
+  padding: 0;
+margin: 0px;
+list-style: none;
+display: flex;
+gap: 1rem;
+}
+li {
+  margin-right: 10px;
+}
+`
+
 
 export default Navbar
